16240f63963cd16ca73af0a3431edc185e00c1e4
JUAN JOSE GARCIA HUACCHA <65090035+JDeveloperu@users.noreply.github.com>
2024-08-08 15:10:18 -0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['tiendasishop.com','cdn.notebookgalerie.de'],
-    },
-};
-
-export default nextConfig;
